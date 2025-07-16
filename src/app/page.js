46056db70d6f969914ab8d9b07182fe8335c20d1
Jarrit Alicea <aliceajarrit@gmail.com>
2025-07-15 23:59:25 -0400

    added back the template and came up with design

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,23 @@
 
-export default function Home() {
 
+export default function Home() {
+	return (
+    <div>
+    <h1>TarotShare</h1>
+
+    <ul>
+    <li>The Different Pulls</li>
+    {/* add card meaning
+        have them be in 2 rows across the page
+        The Differemt Pulls goes in too middle */}
+    <li>Daily Pull</li>
+    <li>Yes/No</li>
+    <li>Three Card Spread</li>
+    <li>Four Card Spread</li>
+    <li>Relationship Spread</li>
+    <li>Moon Phase</li>
+    </ul>
+    
+    </div>
+  )
 }
